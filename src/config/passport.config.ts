adbb80372dcd0265624427c3e8735cac2900fdff
Patrick Rakotoharilalao <patrickrakotoharilalao@gmail.com>
2026-02-14 15:26:04 +0300

    Squashed commit of the following:
    commit 94e50589ced301b36e7e4f5e9819bc66c80698bf
    Author: Patrick Rakotoharilalao <152412702+patrick-rakotoharilalao@users.noreply.github.com>
    Date:   Sat Feb 14 15:24:27 2026 +0300
    
        implementing OAuth Google (#5)
    
        * Clean commits without .env
    
        * update auth.controller.ts

---
+++ src/config/passport.config.ts
@@ -0,0 +1,33 @@
+import passport from 'passport';
+import { Strategy as GoogleStrategy, Profile, VerifyCallback } from 'passport-google-oauth20';
+import { OAuthService } from '../services/oauth.services';
+import { InternalServerError } from '../errors';
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+            callbackURL: process.env.GOOGLE_CALLBACK_URL!,
+        },
+        async (
+            accessToken: string,
+            refreshToken: string,
+            profile: Profile,
+            done: VerifyCallback
+        ) => {
+            try {
+                const loginData = await OAuthService.authenticateWithOAuth(profile, accessToken, refreshToken);
+                done(null, {
+                    loginData
+                });
+            } catch (error) {
+                console.error('Error in Google Strategy:', error);
+                done(error as Error);
+                throw new InternalServerError('Error in Google Strategy');
+            }
+        }
+    )
+);
+
+export default passport;
--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,6 @@ const PORT = Number(process.env.PORT) || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`✅ Server running on port ${PORT}`);
+  console.log(`🔐 Google OAuth: http://localhost:${PORT}/api/auth/google`);
 });
 
